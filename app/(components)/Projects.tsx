a5da0190759fccd02544e5abf57bf91c45b0d4cf
Mohammad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2025-02-08 13:23:05 +0330

    [#18-ADD] new projects (#25)
    * [#18-ADD] new projects
    
    * [#18-UPDATE] experiences

--- app/(components)/Projects.tsx
+++ app/(components)/Projects.tsx
@@ -1,24 +1,31 @@
+import ProjectCard, { ProjectCardModel } from "@/app/(components)/ProjectCard";
+
 function Projects() {
+  const ProjectsData: ProjectCardModel[] = [
+    {
+      image: "kheradkhan",
+      link: "https://github.com/MrAghaei/kheradkhan-v2",
+      buttonText: "Github Repo",
+      title: "KheradKhan",
+    },
+    {
+      image: "medium-clone",
+      link: "https://github.com/MrAghaei/medium-clone",
+      buttonText: "Github Repo",
+      title: "Medium Mock Project",
+    },
+    {
+      image: "novindevtest",
+      link: "https://github.com/MrAghaei/NovinDevTest",
+      buttonText: "Github Repo",
+      title: "NovinDev Interview Project",
+    },
+  ];
   return (
     <div className="dark:bg-darkModeBlack dark:text-white min-h-screen flex flex-col items-center justify-center gap-10 md:gap-20 px-5 md:px-20 lg:px-40 bg-gray-100">
-      <h1 className="text-3xl md:text-5xl font-bold">My Projects</h1>
-      <div
-        data-aos-duration="1000"
-        data-aos="zoom-in"
-        className="shadow-xl hover:!shadow-2xl hover:!transition-shadow hover:!duration-300 flex flex-col justify-between items-center gap-5 border border-blue-900 p-5 rounded-2xl w-full sm:w-3/4 md:w-1/2"
-      >
-        <img
-          alt="kheradkhan"
-          src="/kheradkhan.png"
-          className="w-full h-auto rounded-2xl"
-        />
-        <a
-          href="https://github.com/52-Hertz-Mind/KheradKhan"
-          target="_blank"
-          className="text-center bg-blue-900 w-full sm:w-1/2 text-white p-3 rounded-2xl mt-5 hover:bg-blue-800 duration-300"
-        >
-          Github Repo
-        </a>
+      <h1 className="text-3xl md:text-5xl font-bold mt-24">My Projects</h1>
+      <div className="grid grid-cols-2 gap-8 place-items-center w-3/4 mb-24">
+        <ProjectCard data={ProjectsData} />
       </div>
     </div>
   );
